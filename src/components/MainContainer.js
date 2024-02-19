10526584c423a221ba9f776959f7debfe02d33cc
Lekha <lekhasoni001@gmail.com>
2024-02-19 21:01:55 +0530

    added main continer [vid title , vid bg, sub to store to fetch movies]

---
+++ src/components/MainContainer.js
@@ -0,0 +1,24 @@
+import { useSelector } from "react-redux"
+import VideoBackground from "./VideoBackground"
+import VideoTitle from "./VideoTitle"
+
+
+const MainContainer = () => {
+
+    const movies = useSelector((store) => store.movies?.nowPlayingMovies)
+    if (!movies) return;  //early return 
+
+    const mainMovie = movies[0];
+    console.log(mainMovie)
+
+    const { original_title, overview } = mainMovie;
+
+    return (
+        <div>
+            <VideoTitle title={original_title} overview={overview} />
+            <VideoBackground />
+        </div>
+    )
+}
+
+export default MainContainer; 
