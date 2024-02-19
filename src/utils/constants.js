a25bbd41087794b80e1fd8f8b431fa9702d8fd2a
Lekha <lekhasoni001@gmail.com>
2024-02-19 16:46:06 +0530

    API_options

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,10 @@
 export const LOGO = "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
 export const USE_AVATAR = "https://occ-0-3646-3647.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABTZ2zlLdBVC05fsd2YQAR43J6vB1NAUBOOrxt7oaFATxMhtdzlNZ846H3D8TZzooe2-FT853YVYs8p001KVFYopWi4D4NXM.png?r=229"
+
+export const API_OPTIONS = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5YjZiNjRkOTI4MzBhNzg2NjMyNzZjMjM2ZTBmNmQxOSIsInN1YiI6IjY1ZDMzM2FiNzYxNDIxMDE2M2ZmNjEyYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.9u5Bt848vt87urAZvVrZ8e8XiaMD2UXRzuQsc8uR0Xc'
+    }
+};
