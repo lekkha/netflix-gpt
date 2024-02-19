f2f5936f732e4fe3dfdd9a01915666dca93cb7ad
Lekha <lekhasoni001@gmail.com>
2024-02-19 22:52:23 +0530

    fetching data form video api + storing trialer video key in store + display video

---
+++ src/components/VideoBackground.js
@@ -0,0 +1,39 @@
+import { useEffect } from "react";
+import { API_OPTIONS } from "../utils/constants"
+import { useDispatch, useSelector } from "react-redux";
+import { addTrailerVideo } from "../utils/Slices/movieSlice";
+
+const VideoBackground = ({ movieId }) => {
+    const trailerVideo = useSelector((store) => store.movies?.trailerVideo)
+    const dispatch = useDispatch();
+
+    const getMovieVideo = async () => {
+        const data = await fetch('https://api.themoviedb.org/3/movie/933131/videos?language=en-US', API_OPTIONS)
+        const json = await data.json();
+        console.log(json);
+
+        const filterData = json.results.filter((video) => video.type === "Trailer")
+        const trailer = filterData.length ? filterData[0] : json.results[0];
+        console.log(trailer);
+        dispatch(addTrailerVideo(trailer));
+    }
+
+    useEffect(() => {
+        getMovieVideo();
+    }, [])
+
+    return (
+        <div>
+            <iframe
+                width="560"
+                height="315"
+                src={"https://www.youtube.com/embed/" + trailerVideo?.key}
+                title="YouTube video player"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            >
+            </iframe>
+        </div>
+    )
+}
+
+export default VideoBackground
