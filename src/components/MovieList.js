053afa7657061267506b1f605f5650ac2bccffcc
Lekha <lekhasoni001@gmail.com>
2024-02-20 19:50:03 +0530

    movie list compnanent containing moviecard (via mapping over movies)

---
+++ src/components/MovieList.js
@@ -0,0 +1,19 @@
+import MovieCards from "./MovieCards"
+
+const MovieList = ({ title, movies }) => {
+    console.log(movies)
+
+    return (
+        <div className="px-6">
+            <h1 className="text-xl py-4 text-white">{title}</h1>
+            <div className="flex overflow-x-scroll no-scrollbar">
+                <div className="flex">
+                    {movies?.map((movie) => <MovieCards key={movie.id} posterPath={movie.poster_path} />)}
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default MovieList
