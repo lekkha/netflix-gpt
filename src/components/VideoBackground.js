62886763b06b1c5d19408392ab25d570ccd93f2e
Lekha <lekhasoni001@gmail.com>
2024-02-20 19:50:50 +0530

    css fix

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -11,10 +11,11 @@ const VideoBackground = ({ movieId }) => {
     return (
         <div className="w-screen">
             <iframe
-                className="w-full aspect-video"
+                className="w-screen aspect-video"
                 src={"https://www.youtube.com/embed/" + trailerVideo?.key + "?&autoplay=1&mute=1"}
                 title="YouTube video player"
                 allowFullScreen
+                frameBorder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
             >
             </iframe>
