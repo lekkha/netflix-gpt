113a7b44e8988d173020af7b625a72f8e8947ab7
Lekha <lekhasoni001@gmail.com>
2024-02-20 19:48:08 +0530

    added custom hooks usePopupular,useUpcoming, useTopRated

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -2,11 +2,17 @@ import Header from "./Header";
 import useNowPlayingHook from "../hooks/useNowPlayingHook";
 import MainContainer from "./MainContainer";
 import SecondaryContainer from "./SecondaryContainer";
+import usePopularHook from "../hooks/usePopularHook";
+import useUpcomingHook from "../hooks/useUpcomingHook";
+import useTopRatedHook from "../hooks/useTopRatedHook";
 
 
 const Browse = () => {
 
     useNowPlayingHook();
+    usePopularHook();
+    useUpcomingHook();
+    useTopRatedHook();
 
     return (
         <div>
