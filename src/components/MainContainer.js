281692064592ff87fa8f04521abb2f50ad977173
Lekha <lekhasoni001@gmail.com>
2024-02-20 19:48:23 +0530

    arr 7->6

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -8,7 +8,7 @@ const MainContainer = () => {
     const movies = useSelector((store) => store.movies?.nowPlayingMovies)
     if (!movies) return;  //early return 
 
-    const mainMovie = movies[7];
+    const mainMovie = movies[6];
     console.log(mainMovie)
 
     const { original_title, overview, id } = mainMovie;
