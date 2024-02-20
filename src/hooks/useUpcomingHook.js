03410851baa1e4b1b0a9aeff40b54133c848034e
Lekha <lekhasoni001@gmail.com>
2024-02-20 19:52:06 +0530

    fetches upcoming movie data from api and puts in store

---
+++ src/hooks/useUpcomingHook.js
@@ -0,0 +1,22 @@
+import { useEffect } from "react";
+import { addUpcomingMovies } from "../utils/Slices/movieSlice";
+import { useDispatch } from "react-redux";
+import { API_OPTIONS } from "../utils/constants";
+
+const useUpcomingHook = () => {
+    const dispatch = useDispatch();
+
+    const getUpcomingMovies = async () => {
+        const data = await fetch('https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1', API_OPTIONS);
+        const json = await data.json();
+        // console.log(json.results)
+        dispatch(addUpcomingMovies(json.results))
+
+    }
+
+    useEffect(() => {
+        getUpcomingMovies();
+    }, [])
+}
+
+export default useUpcomingHook;
