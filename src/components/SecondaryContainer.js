c0f60f3318fcd35595bf895550d9d6b943e15f1f
Lekha <lekhasoni001@gmail.com>
2024-02-20 19:50:35 +0530

    cosntains lists of movies (popular upcoming ...)

--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -1,7 +1,22 @@
+import { useSelector } from "react-redux";
+import MovieList from "./MovieList";
 
 const SecondaryContainer = () => {
+    const movies = useSelector(store => store.movies)
+    console.log(movies)
     return (
-        <div>this is the secondary container</div>
+        movies.nowPlayingMovies && (
+            <div className="bg-black">
+                <div className="-mt-28 pl-6 relative z-20">
+                    <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies} />
+                    <MovieList title={"Top Rated"} movies={movies.topRatedMovies} />
+                    <MovieList title={"Upcoming Movies"} movies={movies.upcomingMovies} />
+                    <MovieList title={"Popular"} movies={movies.popularMovies} />
+
+                </div>
+
+            </div>
+        )
     )
 }
 
