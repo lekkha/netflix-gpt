dda56be0066bd0bd73e27b88445cec2b08ec6531
Lekha <lekhasoni001@gmail.com>
2024-02-21 13:39:29 +0530

    added lang for search + placeholder text

---
+++ src/utils/languageConstants.js
@@ -0,0 +1,45 @@
+
+const lang = {
+    en: {
+        search: "search",
+        gptSearchPlaceHolder: "What would you like to watch today?"
+    },
+
+    hindi: {
+        search: "खोज",
+        gptSearchPlaceHolder: "आज आप क्या देखना चाहेंगे?"
+
+    },
+
+    urdu: {
+        search: "تلاش کریں",
+        gptSearchPlaceHolder: "آج آپ کیا دیکھنا چاہیں گے؟"
+    },
+
+    marathi: {
+        search: "शोध",
+        gptSearchPlaceHolder: "तुम्हाला आज काय बघायला आवडेल?"
+    },
+
+    punjabi: {
+        search: "ਖੋਜ",
+        gptSearchPlaceHolder: "ਤੁਸੀਂ ਅੱਜ ਕੀ ਦੇਖਣਾ ਚਾਹੋਗੇ?"
+    },
+
+    bangla: {
+        search: "অনুসন্ধান",
+        gptSearchPlaceHolder: "আপনি আজ কি দেখতে চান?"
+    },
+
+    gujarati: {
+        search: "શોધ",
+        gptSearchPlaceHolder: "તમે આજે શું જોવા માંગો છો?"
+    },
+
+    bhojpuri: {
+        search: "तलाशी",
+        gptSearchPlaceHolder: "आज रउरा का देखल चाहब?"
+    }
+}
+
+export default lang; 
