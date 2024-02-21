ae103f051748df8463f83d806805da8473b2398b
Lekha <lekhasoni001@gmail.com>
2024-02-21 13:38:39 +0530

    added gpt reducer , (lang)config reducer

--- src/utils/appStore.js
+++ src/utils/appStore.js
@@ -1,11 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../utils/Slices/userSlice"
 import moviesReducer from "../utils/Slices/movieSlice"
+import gptReducer from "../utils/Slices/gptSlice"
+import configReducer from "./Slices/configSlice";
 
 const appStore = configureStore({
     reducer: {
         user: userReducer,
         movies: moviesReducer,
+        gpt: gptReducer,
+        config: configReducer,
     },
 });
 
