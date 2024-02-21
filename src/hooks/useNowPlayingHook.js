b9584a7bf83b689546c75481e2b7bbacd09f727b
Lekha <lekhasoni001@gmail.com>
2024-02-22 00:39:10 +0530

    memoization

--- src/hooks/useNowPlayingHook.js
+++ src/hooks/useNowPlayingHook.js
@@ -1,11 +1,12 @@
 import { useEffect } from "react";
 import { addNowPlayingMovies } from "../utils/Slices/movieSlice";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { API_OPTIONS } from "../utils/constants";
 
 const useNowPlayingHook = () => {
     const dispatch = useDispatch();
 
+    const nowPlayingMovies = useSelector((store) => store.movies.nowPlayingMovies);
     const getNowPlayingMovies = async () => {
         const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?page=1', API_OPTIONS);
         const json = await data.json();
@@ -15,7 +16,7 @@ const useNowPlayingHook = () => {
     }
 
     useEffect(() => {
-        getNowPlayingMovies();
+        !nowPlayingMovies && getNowPlayingMovies();
     }, [])
 }
 
