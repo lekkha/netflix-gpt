10ce40204877ed3b149b5ea6ba1b8397c47933e3
Lekha <lekhasoni001@gmail.com>
2024-02-22 00:36:47 +0530

    mapping over movieName and displaying cards

--- src/components/GptMovieSuggestions.js
+++ src/components/GptMovieSuggestions.js
@@ -1,9 +1,26 @@
+import { useSelector } from "react-redux"
+import MovieList from "./MovieList"
+
 
 
 const GptMovieSuggestions = () => {
+
+    const { movieName, movieResults } = useSelector((store) => store.gpt)
+    if (!movieName) return null  //Shimmer UI
+
+
     return (
-        <div>
-            GptMovieSuggestions
+        <div className="mt-10 bg-black text-white bg-opacity-85">
+            <div>
+                {movieName.map((movieName, index) => (
+                    <MovieList
+                        key={movieName}
+                        title={movieName}
+                        movies={movieResults[index]} />
+                ))}
+
+            </div>
+
         </div>
     )
 }
