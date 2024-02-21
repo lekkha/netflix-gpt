61af0f99f33bd160ffbfc4e7e5891d96de76c133
Lekha <lekhasoni001@gmail.com>
2024-02-21 13:37:28 +0530

    BG_IMG constant

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,7 +6,7 @@ import { auth } from "../utils/firebase";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { addUser } from "../utils/Slices/userSlice";
-import { USE_AVATAR } from "../utils/constants";
+import { BG_IMG, USE_AVATAR } from "../utils/constants";
 
 
 const Login = () => {
@@ -41,7 +41,7 @@ const Login = () => {
                             setErrorMessage(error.message)
 
                         });
-                    console.log(user);
+                    //console.log(user);
                 })
                 .catch((error) => {
                     const errorCode = error.code;
@@ -57,7 +57,7 @@ const Login = () => {
                 .then((userCredential) => {
                     // Signed in 
                     const user = userCredential.user;
-                    console.log(user)
+                    //console.log(user)
 
 
                 })
@@ -82,7 +82,7 @@ const Login = () => {
 
             <div className="absolute">
                 <img
-                    src="https://assets.nflxext.com/ffe/siteui/vlv3/fc164b4b-f085-44ee-bb7f-ec7df8539eff/d23a1608-7d90-4da1-93d6-bae2fe60a69b/IN-en-20230814-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+                    src={BG_IMG}
                     alt="bg"
                 />
             </div>
