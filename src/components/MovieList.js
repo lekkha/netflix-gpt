1807d4aa7e1a99667d7fddc74725d8753639c83c
Lekha <lekhasoni001@gmail.com>
2024-02-23 04:44:00 +0530

    added link to movie cards

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,4 +1,5 @@
 import MovieCards from "./MovieCards"
+import { Link } from "react-router-dom"
 
 const MovieList = ({ title, movies }) => {
     //console.log(movies)
@@ -8,7 +9,7 @@ const MovieList = ({ title, movies }) => {
             <h1 className="text-xl py-4 text-white">{title}</h1>
             <div className="flex overflow-x-scroll no-scrollbar">
                 <div className="flex">
-                    {movies?.map((movie) => <MovieCards key={movie.id} posterPath={movie.poster_path} />)}
+                    {movies?.map((movie) => (<Link key={movie.id} to={"/movie/" + movie.id} ><MovieCards key={movie.id} posterPath={movie.poster_path} /></Link>))}
                 </div>
             </div>
 
